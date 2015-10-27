91ebf23aece5dc548a4af4fb7758e8838b36ab04
waxchugdagwad <maxrc@uci.edu>
2015-10-26 20:19:17 -0700

    After a lot of work and compiling from scratch, I think I got a graphics library properly hooked up with the project

--- MGSB/LyricGeneration/Main.cpp
+++ MGSB/LyricGeneration/Main.cpp
@@ -1,10 +1,16 @@
-#include <iostream>
-#include <string>
+#include <pngwriter.h>
 
 int main() {
-	std::cout << "hello world" << std::endl;
-	std::string test;
-	std::cout << test << std::endl;
-	std::cin >> test;
+	int i;
+	int y;
+
+	pngwriter png(300, 300, 0, "test.png");
+	for (int i = 0; i <= 300; i++) {
+		y = 150 + 100 * sin((double)i * 9 / 300.0);
+		png.plot(i, y, 0.0, 0.0, 1.0);
+	}
+
+	png.close();
+
 	return 0;
 }

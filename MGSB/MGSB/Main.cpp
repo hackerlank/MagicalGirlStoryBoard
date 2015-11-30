e3e324df727aeab8a68f521151829dfc10f2fb46
maxrc <maxrc@uci.edu>
2015-11-30 14:43:10 -0800

    Coding on laptop yay

--- MGSB/MGSB/Main.cpp
+++ MGSB/MGSB/Main.cpp
@@ -2,9 +2,23 @@
 #include "Storyboard.hpp"
 #include <iostream>
 #include <string>
+#include "LyricInfoManager.cpp"
+#include <vector>
 
 int main() {
+  std::string lyricPath("..\\LyricGeneration\\Lyrics");
+  std::vector<LyricInfo> lyricInfos = LyricInfoManager::Instance()->Read(lyricPath + "lyricsInfo.txt");
 
+  
+
+  for (auto lyric : lyricInfo) {
+    // Place lyric
+    // Move to it
+    // Move/rotate everything else
+  }
+  
+
+  
 
 	std::string destinationPath = R"(C:\Users\Wax Chug da Gwad\AppData\Local\osu!\Songs\)"
 								  R"(MikitoP ft. Sana - I'm Just an Average Magical Girl, Sorry\)"

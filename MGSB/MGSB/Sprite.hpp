9f7e56021324987842db2b8c0bda31e7f18c7e12
maxrc <maxrc@uci.edu>
2015-11-30 18:14:50 -0800

    Added some code to start with storyboard

--- MGSB/MGSB/Sprite.hpp
+++ MGSB/MGSB/Sprite.hpp
@@ -11,7 +11,7 @@
 
 class Sprite {
 public:
-	Sprite(const std::string& filePath, Vector2 position, Layer layer = Layer::Background, Origin origin = Origin::Centre);
+	Sprite(const std::string& filePath, Vector2 position, Layer layer = Layer::Foreground, Origin origin = Origin::Centre);
 	void Move(int startTime, int endTime, int startX, int startY, int endX, int endY, Easing easing = Easing::Linear);
 	void Fade(int startTime, int endTime, double startOpacity, double endOpacity, Easing easing = Easing::Linear);
 	void Rotate(int startTime, int endTime, double startRotate, double endRotate, Easing easing = Easing::Linear);
